c485495bdd4960b3499c45b747bd01371f214933
mariadhantia <mariadhantia@gmail.com>
2022-08-05 00:56:15 +0500

    my change

--- server.mjs
+++ server.mjs
@@ -1,11 +1,26 @@
 
 import express from 'express'
-const app = express()
-const port = 3000
+import cors from 'cors';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express();
+app.use(cors());
+const port = 4000;
+
+app.get('/home', (req, res) => {
+  res.send('I am home page!')
 })
+
+app.get('/profile', (req, res) => {
+    res.send('I am profile page!')
+  })
+  app.get('/weather', (req, res) => {
+    res.send({
+        weather: "sunny",
+        temprature:"30",
+        condition:"clear",
+        city:"karachi",
+    })
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
